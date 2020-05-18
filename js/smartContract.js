385ecfedb85b96288cd9518fa0c47b1bd31a7903
Emanuel Castillo <ecastillo76@misena.edu.co>
2020-05-18 20:32:29 +0200

    settings

--- js/smartContract.js
+++ js/smartContract.js
@@ -1,45 +1,30 @@
 // Button "Invertir Ahora"
-const investButton = document.querySelector('#investButton')
+const btnInvest = document.querySelector('#btn-invest')
+const btnVer = document.querySelector('#btn-ver')
+const btnCode = document.querySelector('#btn-code')
+
+
 const agree = document.querySelector('#agree')
 const wallet = document.querySelector('#wallet')
 const amountOptions = document.querySelector('#amountOptions')
 
-const amounts = [0, 500, 1000, 2000, 5000, 10000, 25000, 50000, 100000, 200000, 500000, 1000000]
 
 
 
-investButton.onclick = () => {
+btnInvest.onclick = () => {
  if(agree.checked) {
-
-  if(wallet.value == ''){
-
-    alert('Ingresa una dirección de ETH válida')
-
-  } else {
-
-    if(amountOptions.options[0].selected) {
-      alert('Por favor selecciona una cantidad a invertir')
-
-    } else {
-      
-      let amount
-
-      for(let i = 0; i <= amountOptions.length; i++) {
-
-        if(amountOptions.options[i].selected) {
-
-          amount = amounts[i]
-          break
-        }
-        window.open('https://etherscan.io/address/0xc9652efc267982b82df4ffd4bba5e0e7392f51f5#code')
-      }
-      
-      console.log(amount)
-    }
-  }
-
+   window.open('https://etherscan.io/address/0xc9652efc267982b82df4ffd4bba5e0e7392f51f5#writeContract')
  } else {
   alert('Por favor selecciona la casilla e indica que has descargado y leído Las Cláusulas del Contrato')
-
  }
 }
+
+btnVer.onclick = () => {
+  window.open('https://etherscan.io/address/0xc9652efc267982b82df4ffd4bba5e0e7392f51f5#readContract')
+}
+
+btnCode.onclick = () => {
+  window.open('https://etherscan.io/address/0xc9652efc267982b82df4ffd4bba5e0e7392f51f5#code')
+}
+
+
